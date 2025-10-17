d82b80a121ac9d2a655df3d938157c681d974271
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-18 02:11:35 +0300

    Added Data Fetching to EventsPage (6) : You can't Access Data returned by in Higher Level Component

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -4,8 +4,8 @@ import {Outlet, useLoaderData} from "react-router-dom";
 export default function Layout() {
     //! You can't read loader data in a higher level component
     //! You can access loader data in the same level or any lower level component
-    const events = useLoaderData();
-    console.log(events)
+    // const events = useLoaderData();
+    // console.log(events)
     return (
         <>
             <MainNavigation/>
