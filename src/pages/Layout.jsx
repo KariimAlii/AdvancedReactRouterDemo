a3a1d4ff62b0a4d293004de66baf982a2cc4da7d
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-18 02:44:53 +0300

    Reflect the current Navigation Transition State on the UI

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,15 +1,20 @@
 ﻿import MainNavigation from "../components/MainNavigation.jsx";
-import {Outlet, useLoaderData} from "react-router-dom";
+import {Outlet, useLoaderData, useNavigation} from "react-router-dom";
 
 export default function Layout() {
     //! You can't read loader data in a higher level component
     //! You can access loader data in the same level or any lower level component
     // const events = useLoaderData();
     // console.log(events)
+
+    //! useNavigation() hook is used to get information about the current active navigation transition
+    const navigation = useNavigation();
+    //! navigation.state ===> 'idle' || 'loading' || 'submitting';
     return (
         <>
             <MainNavigation/>
             <main>
+                {navigation.state === 'loading' && <p>Loading.....</p>}
                 <Outlet />
             </main>
         </>
