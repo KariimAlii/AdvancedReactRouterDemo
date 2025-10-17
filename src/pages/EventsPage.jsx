127489cd3f0ad7411e4bd6afac79032d823a7c94
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-18 01:59:33 +0300

    Added Data Fetching to EventsPage (1)

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,21 +1,36 @@
-﻿import {Link} from "react-router-dom";
+﻿import { useEffect, useState } from 'react';
 
-const DUMMY_EVENTS = [
-    { id : '1', title: 'Event 1'},
-    { id : '2', title: 'Event 3'},
-    { id : '3', title: 'Event 3'},
-]
+import EventsList from '../components/EventsList';
+
 export default function EventsPage() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [fetchedEvents, setFetchedEvents] = useState();
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        async function fetchEvents() {
+            setIsLoading(true);
+            const response = await fetch('http://localhost:8080/events');
+
+            if (!response.ok) {
+                setError('Fetching events failed.');
+            } else {
+                const resData = await response.json();
+                setFetchedEvents(resData.events);
+            }
+            setIsLoading(false);
+        }
+
+        fetchEvents();
+    }, []);
     return (
         <>
-            <h1>Events Page</h1>
-            <ul>
-                {DUMMY_EVENTS.map(event => (
-                    <li key={event.id}>
-                        <Link to={event.id}>{event.title}</Link>
-                    </li>
-                ))}
-            </ul>
+            <div style={{ textAlign: 'center' }}>
+                {isLoading && <p>Loading...</p>}
+                {error && <p>{error}</p>}
+            </div>
+            {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
         </>
-    )
+    );
 }
+
