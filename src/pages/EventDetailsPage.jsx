d023d6c630dc04d8fe7f60b2c37bf362614dab5f
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-18 01:48:38 +0300

    Added Navigation from Events Page to EventDetails Page and backwards

--- src/pages/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage.jsx
@@ -1,3 +1,13 @@
-﻿export default function EventDetailsPage() {
-    return <h1>Event Details Page</h1>
+﻿import {Link, useParams} from "react-router-dom";
+
+export default function EventDetailsPage() {
+    const {eventId} = useParams();
+
+    return (
+        <>
+            <h1>Event Details Page</h1>
+            <p>Event {eventId}</p>
+            <Link to=".." relative="path">Back to Index</Link>
+        </>
+    )
 }
--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,3 +1,21 @@
-﻿export default function EventsPage() {
-    return <h1>Events Page</h1>
+﻿import {Link} from "react-router-dom";
+
+const DUMMY_EVENTS = [
+    { id : '1', title: 'Event 1'},
+    { id : '2', title: 'Event 3'},
+    { id : '3', title: 'Event 3'},
+]
+export default function EventsPage() {
+    return (
+        <>
+            <h1>Events Page</h1>
+            <ul>
+                {DUMMY_EVENTS.map(event => (
+                    <li key={event.id}>
+                        <Link to={event.id}>{event.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
