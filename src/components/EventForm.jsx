174ec78bafc6896b45ccdfc3f457da378cbc6baf
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-18 13:46:17 +0300

    Code Refactoring

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -25,19 +25,19 @@ function EventForm({ method, event }) {
         )}
       <p>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" defaultValue={event && event.title} />
+        <input id="title" type="text" name="title" required defaultValue={event && event.title} />
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" defaultValue={event && event.image} />
+        <input id="image" type="url" name="image" required defaultValue={event && event.image} />
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" defaultValue={event && event.date} />
+        <input id="date" type="date" name="date" required defaultValue={event && event.date} />
       </p>
       <p>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" defaultValue={event && event.description} />
+        <textarea id="description" name="description" rows="5" required defaultValue={event && event.description} />
       </p>
       <div className={classes.actions}>
         <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
