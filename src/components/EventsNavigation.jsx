b9a3a732629e5666f58a98370108274c5ce25ba9
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-25 01:05:48 +0300

    Show/Hide page content based on user authentication

--- src/components/EventsNavigation.jsx
+++ src/components/EventsNavigation.jsx
@@ -1,7 +1,8 @@
 import classes from './styles/EventsNavigation.module.css';
-import {NavLink} from "react-router-dom";
+import {NavLink, useRouteLoaderData} from "react-router-dom";
 
 function EventsNavigation() {
+  const token = useRouteLoaderData('root');
   return (
     <header className={classes.header}>
       <nav>
@@ -13,12 +14,14 @@ function EventsNavigation() {
                 end
             >All Events</NavLink>
           </li>
-          <li>
-            <NavLink
-                to="/events/new"
-                className={({isActive}) => isActive ? classes.active : null}
-            >New Event</NavLink>
-          </li>
+          {token && (
+              <li>
+                <NavLink
+                    to="/events/new"
+                    className={({isActive}) => isActive ? classes.active : null}
+                >New Event</NavLink>
+              </li>
+          )}
         </ul>
       </nav>
     </header>
